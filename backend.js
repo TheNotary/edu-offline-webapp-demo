50d4119d539a21b7817d8904d759236d2477d041
TheNotary <no@email.plz>
2022-07-20 16:16:48 -0500

    fleshes out backend

--- backend.js
+++ backend.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://mongoadmin:secret@localhost/';
-var str = "";
+
+let databaseName = "myDatabase";
+let collectionName = "records";
+
+
+///////////////////
+// Configuration //
+///////////////////
+//
+
+// https://stackoverflow.com/questions/24330014/bodyparser-is-deprecated-express-4/24330353#24330353
+app.use(express.json());
+app.use(express.urlencoded());
+
 
 // serve static html assets in the sw-test/ folder
 app.use('/sw-test', express.static('sw-test'));
@@ -14,66 +27,132 @@ app.get('/', function(req, res){
 
 app.route('/health').get(function(req, res) {
     console.log("Responding to /health");
-    res.send("online");
+    res.send("up");
 });
 
-app.route('/seed').get(function(req, res) {
-    console.log("Responding to /seed");
+app.route('/setup').get(function(req, res) {
+    console.log("Responding to /setup");
+
+    MongoClient.connect(url, function(err, mongoclient) {
+        setupDb(err, mongoclient, res);
+    });
+});
+
+app.route('/records').get(function(req, res) {
+    console.log("Responding to /records");
 
     MongoClient.connect(url, function(err, mongoclient) {
         presentAllRecords(err, mongoclient, res);
     });
 });
 
-app.route('/Employeeid').get(function(req, res) {
-    console.log("Request Received.");
+app.route('/records').post(function(req, res) {
+    console.log("Responding to POST /records");
+
+    var newRecord = {
+        capacity: req.body.capacity,
+        checked: req.body.checked,
+        data: req.body.data
+    };
+    var _id = req.body._id;
 
     MongoClient.connect(url, function(err, mongoclient) {
-        console.log("Connected");
-
-        var cursor = mongoclient.collection('Employee').find();
-        //noinspection JSDeprecatedSymbols
-        cursor.each(function(err, item) {
-
-            if (item != null) {
-                str = str + "    Employee id  " + item.Employeeid + "</br>";
-            }
-        });
-        res.send(str);
-        mongoclient.close();
+        mongoclient
+            .db(databaseName)
+            .collection(collectionName)
+            .replaceOne(
+                { _id },
+                newRecord
+            ).then( _ => {
+                mongoclient.close();
+                res.send("ok");
+            });
     });
 });
 
-var server = app.listen(3000, function() {
-    console.log("Listening on port 3000");
+app.route('/records/count').get(function(req, res) {
+    console.log("Responding to /records/count");
 
+    MongoClient.connect(url, function(err, mongoclient) {
+        presentCount(err, mongoclient, res);
+    });
 });
 
 
+var server = app.listen(3000, function() {
+    console.log("Listening on port 3000");
+});
 
-async function getCount(db) {
-    let count = await db.collection('records').countDocuments();
 
-    return count;
+async function presentCount(err, mongoclient, res) {
+    let count = await getCount(mongoclient);
+    res.send('' + count);
+}
+
+async function getCount(mongoclient) {
+    return mongoclient
+        .db(databaseName)
+        .collection(collectionName)
+        .countDocuments();
+}
+
+
+async function setupDb(err, mongoclient, response) {
+    console.log("Setup Db");
+
+    let data = 'a'.repeat(500000);
+
+    let seedData = [
+        { _id: 'record0001', capacity: 3, checked: false, data: data },
+        { _id: 'record0002', capacity: 3, checked: false, data: data},
+        { _id: 'record0003', capacity: 3, checked: false, data: data},
+        { _id: 'record0004', capacity: 3, checked: false, data: data},
+    ];
+
+    let db = mongoclient.db(databaseName);
+
+    // Clear the database first in case it's been populated before
+    await db
+        .collection(collectionName)
+        .drop(function(err, delOK) {
+            console.log("doing drop");
+
+            if (err && err.codeName == 'NamespaceNotFound') {
+                console.log("Namespace not found, no need to drop");
+                return;
+            }
+
+            console.log("got here unexpectadly");
+            if (err) throw err;
+            if (delOK) console.log("Collection deleted");
+            if (!delOK) console.log("Something went wrong deleting the collection?");
+        });
+
+    // Then seed it
+    await db
+        .collection(collectionName)
+        .insertMany(seedData, function(err, res) {
+            console.log("doing insert");
+            if (err) throw err;
+            console.log("Number of documents inserted: " + res.insertedCount);
+
+            mongoclient.close();
+            response.send("ok");
+        });
+
+        console.log("Finished the awaits");
 }
 
 async function presentAllRecords(err, mongoclient, res) {
     console.log("Connected");
 
-    let db = mongoclient.db("myDatabase");
-    let count = await getCount(db);
-    db
-        .collection('records')
+    mongoclient
+        .db(databaseName)
+        .collection(collectionName)
         .find({})
-        .forEach(function(err, item) {
-            if (item != null) {
-                str = str + "    Employee id  " + item.Employeeid + "</br>";
-            }
-        })
-        .then( _ => {
-            var what = "hihi";
-            res.send(str);
+        .toArray()
+        .then( array => {
+            res.send(JSON.stringify(array));
             mongoclient.close();
         });
-
 }
